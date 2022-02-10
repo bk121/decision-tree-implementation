b88a11aea2b69b03f4879e756e56263f3a5840ed
Jonas Birk <jonasbirk1996@gmail.com>
2022-02-10 20:56:10 +0000

    implement cross_validation_prediction()
    Former-commit-id: 3c93d1def34b832eed26c90927ad80355397a585

--- cross_validation.py
+++ cross_validation.py
@@ -1,3 +1,17 @@
+"""
+File:           cross_validation.py
+Author:         Jonas Birk, Ted Jenks, Tom Mitcheson, Ben Kirwan
+Creation Date:  31/01/2022
+Last Edit Date: 10/02/2022
+Last Edit By:   Ted Jenks
+
+Public Functions:   k_fold_split(n_splits, n_instances), 
+                    train_test_k_fold(decision_tree_classifier, train_attr, train_labels, test_attr, test_labels, n_splits)
+
+Summary of File:
+
+        Contains functions for performing cross-validation on classifier
+"""
 import numpy as np
 
 def k_fold_split(n_splits, n_instances):
@@ -32,14 +46,16 @@ def train_test_k_fold(decision_tree_classifier, train_attr, train_labels, test_a
         folds.append(index_set)
 
     # Currently, folds[] has 10 rows (as per n_splits argument) and 2 columns;
-    # first column is train_indices and 2ndd is test_indices
+    # first column is train_indices and 2nd is test_indices
     accuracies = []
-    
+    fitted_classifiers = []
+
     for i in range(n_splits):
         x_train = full_attr[folds[i][0]]
         y_train = full_labels[folds[i][0]]
 
         decision_tree_classifier.fit(x_train, y_train)
+        fitted_classifiers.append(decision_tree_classifier)
 
         x_test = full_attr[folds[i][1]]
         y_test = full_labels[folds[i][1]]
@@ -49,4 +65,4 @@ def train_test_k_fold(decision_tree_classifier, train_attr, train_labels, test_a
         accuracy = (np.count_nonzero(predictions == y_test)) / y_test.size
         accuracies.append(accuracy)
 
-    return (np.mean(accuracies), np.std(accuracies))
+    return accuracies, fitted_classifiers
